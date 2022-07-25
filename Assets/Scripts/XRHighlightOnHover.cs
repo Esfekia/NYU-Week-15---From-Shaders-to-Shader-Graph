64c0b8f36b6d66dfea4c0c85aaf2597f86b9a699
Esfekia <sertac@gmail.com>
2022-07-25 06:52:11 -0400

    Our XRHighlightOnHover script was added to all interactable objects, XR Rig added to scene as well as a XR On Look Controller and now we are able to highlight objects using our shader by looking at them
    Our XRHighlightOnHover script was added to all interactable objects, XR Rig added to scene as well as a XR On Look Controller and now we are able to highlight objects using our shader by looking at them

---
+++ Assets/Scripts/XRHighlightOnHover.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRHighlightOnHover : MonoBehaviour
+{
+
+    public Material hoverMaterial = null;
+    XRBaseInteractable m_interactable = null;
+
+    Renderer m_renderer = null;
+    Material[] m_currentMaterials = null;
+
+    void Awake()
+    {
+        m_renderer = GetComponent<Renderer>();
+        m_interactable = GetComponent<XRBaseInteractable>();
+    }
+
+    private void OnEnable()
+    {
+        m_interactable.onHoverEnter.AddListener(SwapInMaterial);
+        m_interactable.onLastHoverExit.AddListener(SwapOutMaterial);
+    }
+
+    private void OnDisable()
+    {
+        m_interactable.onHoverEnter.RemoveListener(SwapInMaterial);
+        m_interactable.onLastHoverExit.RemoveListener(SwapOutMaterial);
+    }
+
+    void SwapInMaterial(XRBaseInteractor interactor)
+    {
+        m_currentMaterials = m_renderer.materials;
+        Material[] hoverMats = new Material[m_currentMaterials.Length];
+        for (int i= 0; i < m_currentMaterials.Length; ++i)
+        {
+            hoverMats[i] = hoverMaterial;
+        }
+        m_renderer.materials = hoverMats;
+    }
+    void SwapOutMaterial(XRBaseInteractor interactor)
+    {
+        m_renderer.materials = m_currentMaterials;
+        m_currentMaterials = null;
+    }
+}
